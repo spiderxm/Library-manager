bc6fd16ac5054c559483de85cf0286c4a3746462
Shwetagurnani <sainikhyati2001@gmail.com>
2020-05-28 19:20:28 +0530

    Files changed

--- app/src/main/java/com/example/librarymanager/AddbookActivity.java
+++ app/src/main/java/com/example/librarymanager/AddbookActivity.java
@@ -4,12 +4,17 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import java.util.Date;
+
 public class AddbookActivity extends AppCompatActivity {
 
-    Button back;
+    Button back, addBook;
+    EditText bookName, authorName, bookId, issueDate, department;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -17,12 +22,63 @@ public class AddbookActivity extends AppCompatActivity {
         setContentView(R.layout.activity_addbook);
 
         back = findViewById(R.id.buttonAddBack);
+        addBook = findViewById(R.id.buttonAddBook);
+        bookName = findViewById(R.id.editTextBookName);
+        authorName = findViewById(R.id.editTextAuthorName);
+        bookId = findViewById(R.id.editTextBookId);
+        issueDate = findViewById(R.id.editTextIssueDate);
+        department = findViewById(R.id.editTextDepartment);
 
         back.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 Intent intent = new Intent(AddbookActivity.this, HomeActivity.class);
                 startActivity(intent);
+            }
+        });
+
+        addBook.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                String nameBook = bookName.getText().toString();
+                String nameAuthor = authorName.getText().toString();
+                String idBook = bookId.getText().toString();
+                String dateIssue = issueDate.getText().toString();
+                String dept = department.getText().toString();
+
+                if(nameBook.isEmpty() && nameAuthor.isEmpty() && idBook.isEmpty() && dateIssue.isEmpty() && dept.isEmpty())
+                {
+                    Toast.makeText(getApplicationContext(), "Please fill the details", Toast.LENGTH_SHORT).show();
+                }
+                else
+                {
+                    if(nameBook.isEmpty())
+                    {
+                        bookName.setError("Please enter Book Name");
+                        bookName.requestFocus();
+                    }
+                    if(nameAuthor.isEmpty())
+                    {
+                        authorName.setError("Please enter Author Name");
+                        authorName.requestFocus();
+                    }
+                    if(idBook.isEmpty())
+                    {
+                        bookId.setError("Please enter Book ID");
+                        bookId.requestFocus();
+                    }
+                    if(dateIssue.isEmpty())
+                    {
+                        issueDate.setError("Please enter Issue Date");
+                        issueDate.requestFocus();
+                    }
+                    if(dept.isEmpty())
+                    {
+                        department.setError("Please enter Department");
+                        department.requestFocus();
+                    }
+                }
             }
         });
 
--- app/src/main/java/com/example/librarymanager/ContributorsActivity.java
+++ app/src/main/java/com/example/librarymanager/ContributorsActivity.java
@@ -1,6 +1,7 @@
 package com.example.librarymanager;
 
 import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
@@ -9,7 +10,7 @@ import androidx.appcompat.app.AppCompatActivity;
 
 public class ContributorsActivity extends AppCompatActivity {
 
-    Button back;
+    Button back, GitHubProfile1, GitHubProfile2;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -17,11 +18,31 @@ public class ContributorsActivity extends AppCompatActivity {
         setContentView(R.layout.activity_contributors);
 
         back = findViewById(R.id.buttonContBack);
+        GitHubProfile1 = findViewById(R.id.buttonLink1);
+        GitHubProfile2 = findViewById(R.id.buttonLink2);
 
         back.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 Intent intent = new Intent(ContributorsActivity.this, HomeActivity.class);
+                startActivity(intent);
+            }
+        });
+
+        GitHubProfile1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Uri uri = Uri.parse("https://github.com/KhyatiSaini");
+                Intent intent = new Intent(Intent.ACTION_VIEW, uri);
+                startActivity(intent);
+            }
+        });
+
+        GitHubProfile2.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Uri uri = Uri.parse("https://github.com/spiderxm");
+                Intent intent = new Intent(Intent.ACTION_VIEW, uri);
                 startActivity(intent);
             }
         });
