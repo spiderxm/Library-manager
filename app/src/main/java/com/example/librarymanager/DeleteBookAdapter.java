c70decfcfe2ef213826294df76dbfe089c0763de
Shwetagurnani <sainikhyati2001@gmail.com>
2020-06-22 15:50:57 +0530

    added adapter

--- app/src/main/java/com/example/librarymanager/DeleteBookAdapter.java
+++ app/src/main/java/com/example/librarymanager/DeleteBookAdapter.java
@@ -8,8 +8,6 @@ import android.widget.TextView;
 import androidx.annotation.NonNull;
 import androidx.recyclerview.widget.RecyclerView;
 
-import org.w3c.dom.Text;
-
 import java.util.Date;
 import java.util.List;
 
